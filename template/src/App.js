caaa31229a6f97cd08170d25172f509c7a7fa983
henriquelbk <loubackhenrique123@gmail.com>
2023-05-10 21:15:39 -0300

    ain

--- template/src/App.js
+++ template/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
+import Garagem from "./componentes/Garagem";
+
 
 export default function App() {
   
-
   return (
+    <>
     <div className="App">
-      
+      <Garagem />
      <h1>Aula - Componentes React</h1>
     </div>
+    </>
   );
 }
---
+++ template/src/componentes/Carro.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Carro = () => {
+    return (
+        <>
+            <h2>Monza</h2>
+            <ul>
+                <li>Cor: rosa</li>
+                <li>Ano: 1987</li>
+                <li>Flex: não</li>
+            </ul>
+        </>
+    );
+};
+
+export default Carro
